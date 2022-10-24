ede58593c4d28ceedc4b2119a021bf2a522f9bcd
joseph <j.hernandez.92189@gmail.com>
2022-10-24 11:38:10 -0700

    added base schemas

--- schemas/schemas.sql
+++ schemas/schemas.sql
@@ -1,2 +1,27 @@
 DROP DATABASE IF EXISTS store_db;
 CREATE DATABASE store_db;
+
+USE store_db;
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE 
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    manager_id INTEGER,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
---
+++ schemas/seeds.sql
@@ -0,0 +1,8 @@
+USE store_db;
+
+INSERT INTO department (name) 
+VALUES 
+('Sales'),
+('Finance'),
+('Human Resources'),
+('Operations');
