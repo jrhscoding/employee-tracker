f8dd2a015200b2d19f9a6cf1aa61d4dbf0e0c201
joseph <j.hernandez.92189@gmail.com>
2022-10-24 12:13:50 -0700

    removed non working inquirer prompts

--- index.js
+++ index.js
@@ -13,14 +13,6 @@ const originalPrompts = () => {
                     'View all departments',
                     'View all roles',
                     'View all employees',
-                    'Add a department',
-                    'Delete a department',
-                    'Add a role',
-                    'Delete a role',
-                    'Add an employee',
-                    'Delete an employee',
-                    "Update an employee's role",
-                    "Update an employee's manager",
                     'Quit'
                 ]
             }
